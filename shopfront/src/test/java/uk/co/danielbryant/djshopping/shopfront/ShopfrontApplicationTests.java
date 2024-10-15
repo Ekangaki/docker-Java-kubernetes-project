885670467781552ec57304055bbb11f3046d95e8
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-10-15 10:53:26 -0400

    Update ShopfrontApplicationTests.java

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
@@ -6,9 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShopfrontApplication.class)
+@SpringBootTest(classes = ShopfrontApplication.class)
+@ActiveProfiles("test")
 public class ShopfrontApplicationTests {
-
+    
     @Test
     public void contextLoads() {
     }
